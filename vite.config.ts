bcc94bf1f30f46a349171ba97b024668eb174c77
yonghang <1251679661@qq.com>
2026-05-28 17:02:50 +0800

    ci(github-pages): 添加 GitHub Pages 部署配置并调整 Vite 基础路径

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const isGithubPages = process.env.GITHUB_ACTIONS === 'true'
+const repoBase = '/vue-nginx-zero-downtime/'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: isGithubPages ? repoBase : '/',
   plugins: [vue()],
 })
